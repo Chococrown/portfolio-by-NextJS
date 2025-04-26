13da1c04c461ea0e1cdaae9db15571cda9475329
Chococrown <127465228+Chococrown@users.noreply.github.com>
2025-04-26 08:45:56 +0700

    เพิ่ม Footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,20 @@
+import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
+
 
 export default function Home() {
   return (
            <>
+              <div className = 'container'>
 
-            <Navbar />
+                <Navbar />
 
-            <h1>หน้าแรกของเว็บไซต์</h1>
-            <h2>Hello World</h2>
-           
+                <h1>หน้าแรกของเว็บไซต์</h1>
+                <h2>Hello World</h2>
+
+                <Footer />  
+
+              </div>
            </>
   );
 }
---
+++ components/Footer.tsx
@@ -0,0 +1,13 @@
+
+
+export default function Footer() {
+    return ( 
+        <>
+            <div className='footer'> 
+
+                &copy; 2025. ประเสริฐ ตุ่นก่อ. สงวนสิทธิ์. 
+
+            </div>
+         </>
+    );
+}
